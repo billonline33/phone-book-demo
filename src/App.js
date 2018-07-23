e3f2d8992b7c59005ae31a875e7fa0b6752eadce
Bill Huang <billonline33@hotmail.com>
2018-07-23 11:38:21 +1000

    feature(redux): integrate redux
    install redux
    install react-redux
    install reudx-thunk
    create redux-modules folder
        create emplyee reducer
        create combined reducer
    create store

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+import "./App.css";
 import InputForm from "./components/InputForm/InputForm";
 import EmployeeTable from "./components/EmployeeTable/EmployeeTable";
 
 class App extends Component {
   render() {
+    console.log("this.props.employeeList: ", this.props.employeeList);
     return (
       <div className="App">
-        <InputForm/>
-        <EmployeeTable/>
+        <InputForm />
+        <EmployeeTable />
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => ({
+  employeeList: state.employees.employeeList
+});
+
+const mapDispatchToProps = dispatch => bindActionCreators({}, dispatch);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
---
+++ src/redux-modules/employees.js
@@ -0,0 +1,15 @@
+
+const initialState = {
+  employeeList: [],
+  isEditingEmployee: false,
+  sortFirstName: null,
+  sortLastName: null,
+  sortPhoneNumber: null
+}
+
+export default (state=initialState, action) => {
+  switch(action.type) {
+    default:
+      return state
+  }
+}
