a286c70bf0f09a17698c32aaf7ebc2d4aa7ae52e
billonline33 <billonline33@hotmail.com>
2018-08-29 13:43:45 +1000

    feature: add spinner during loading

--- src/hoc/LoaderHOC.js
+++ src/hoc/LoaderHOC.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import './loader.css';
 const LoaderHOC = WrappedComponent => {
   class HOC extends Component {
     render() {
       console.log('9999 this.props in HOC=', this.props);
-      return <WrappedComponent {...this.props} />;
+      return this.props.employeeList.length === 0 ? (
+        <div className="loader" />
+      ) : (
+        <WrappedComponent {...this.props} />
+      );
     }
   }
   return HOC;
